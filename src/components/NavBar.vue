9358c384a647a36b38ec88243fce4e175b6036ac
Kevin Liu <kliu@southtexascollege.edu>
2022-09-21 22:44:39 -0500

    protected route

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,32 @@
 <script setup>
   import { ref } from 'vue'
-  const brand = ref('🏢 Fake Company Directory ')
+  import { useAuth } from '@/composables/useAuth'
+
+  const { isAuthenticated, logout, user } = useAuth()
+
+  const brand = ref('🏢 Fake Company Directory')
 </script>
+
 <template>
   <nav>
-    <div class="wraper">
+    <div class="wrapper">
       <RouterLink :to="{ name: 'Home' }" class="brand">
         <span class="brand-title">{{ brand }}</span>
-        </RouterLink>
-      </div>
+      </RouterLink>
       <div class="menu">
-        <a href="#" class="menu-item">Departments</a>
-        <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-login">Logout</a>
+        <p v-show="isAuthenticated" class="px-2 py-4">
+          Welcome back
+          <strong
+            ><i>{{ user.name }}</i></strong
+          >
+        </p>
+        <div v-if="isAuthenticated">
+          <RouterLink :to="{ name: 'Settings' }" href="#" class="menu-item">Settings</RouterLink>
+          <button class="menu-logout" @click="logout">Logout</button>
+        </div>
+        <div v-else>
+          <RouterLink :to="{ name: 'Login' }" href="#" class="menu-login">Login</RouterLink>
+        </div>
       </div>
     </div>
   </nav>
@@ -20,9 +34,9 @@
 
 <style scoped lang="postcss">
   nav {
-    @apply flex h-20 items-center justify-between bg-slate-900 text-slate-200;
-    .wraper {
-      @apply container mx-auto flex w-full justify-between justify-items-center;
+    @apply flex h-20 bg-slate-900 text-slate-200;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
       .brand {
         &-title {
           @apply text-2xl font-bold text-yellow-500;
@@ -30,11 +44,17 @@
       }
       .menu {
         @apply flex gap-2;
+        & div {
+          @apply py-2;
+        }
         &-item {
-          @apply rounded-md px-4 py-2  hover:bg-yellow-500 hover:text-slate-900;
+          @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
         }
         &-login {
-          @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+          @apply rounded-md bg-green-500 px-4 py-2 text-red-100 hover:bg-green-700;
+        }
+        &-logout {
+          @apply mx-2 rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
         }
       }
     }
