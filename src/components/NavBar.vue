1e630d63771698d34c928e968486f6f7b3f1027d
Kevin Liu <kliu@southtexascollege.edu>
2022-09-26 17:10:03 -0500

    added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
 
   const { isAuthenticated, logout, user } = useAuth()
 
-  const brand = ref('🏢 Fake Company Directory')
+  const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
